966596d691b3f7ed84a654a541d9cbaccd405862
danzo02 <danzola02@gmail.com>
2025-12-08 12:48:27 -0700

    Updated loginView with shadcn components

--- src/views/loginView.jsx
+++ src/views/loginView.jsx
@@ -1,37 +1,62 @@
-import { useContext } from 'react';
-import { LoginContext } from '../context/loginContext';
-import { useNavigate } from 'react-router'; 
+import { useContext } from "react";
+import { useNavigate } from "react-router";
+import { LoginContext } from "../context/loginContext";
+import {Card,CardHeader,CardTitle,CardContent,CardFooter,} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {Field,FieldGroup,FieldLabel} from "@/components/ui/field";
 
 const LoginView = () => {
   const { login } = useContext(LoginContext);
   const navigate = useNavigate();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    login();         // admin logged in
-    navigate('/');   // redirect to Home
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login();
+    navigate("/");
   };
 
-
   return (
-    <div>
-      <h2>Admin Login</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username:
-          <input type="text" defaultValue="admin" />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input type="password" defaultValue="password" />
-        </label>
-        <br />
-        <button type="submit">Login</button>
-        <button type="button">Register</button>
-      </form>
+    <div className="flex justify-center mt-20">
+      <Card className="w-[350px] shadow-lg">
+        <CardHeader>
+          <CardTitle className="text-center">Admin Login</CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <form onSubmit={handleSubmit}>
+            <FieldGroup className="space-y-4">
+              <Field>
+                <FieldLabel htmlFor="username">Username</FieldLabel>
+                <Input id="username" defaultValue="admin" required />
+              </Field>
+
+
+              <Field>
+                <FieldLabel htmlFor="password">Password</FieldLabel>
+                <Input
+                  id="password"
+                  type="password"
+                  defaultValue="password"
+                  required
+                />
+              </Field>
+
+              <Button type="submit" className="w-full mt-2">
+                Login
+              </Button>
+            </FieldGroup>
+          </form>
+        </CardContent>
+
+        <CardFooter>
+          <Button variant="outline" className="w-full">
+            Register
+          </Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 };
 
-export {LoginView};
+export { LoginView };

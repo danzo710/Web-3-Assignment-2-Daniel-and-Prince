aefb4e4a41702955b9c0c31639ad81c87f7a6511
danzo02 <danzola02@gmail.com>
2025-12-07 19:42:16 -0700

    added browse route

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from 'react-router';
 import HomeView from '../views/homeView';
 import LoginView from '../views/loginView';
+import BrowseView from '../views/browse';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={< HomeView />} />
       <Route path="/login" element={< LoginView />}/>
+      <Route path="/browse" element={< BrowseView />}/>
       {/* <Route path="/dashboard/sales" element={}/>
-      <Route path="/browse" element={}/>
       <Route path="/product/men" element={}/>
       <Route path="/product/woman" element={}/>
       <Route path="/cart" element={}/>
